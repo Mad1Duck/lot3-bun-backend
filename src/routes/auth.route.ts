eed7e59a4d758dc0f623cf6816db0d50039e5101
aqua-marine-321 <aqua-marine@gmx.com>
2025-05-26 15:44:52 +0700

    add refresh logout

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -2,10 +2,12 @@
 import { Hono } from 'hono';
 import { validate } from '@/middleware/zod.middleware';
 import { loginSchema, registerSchema } from '@/validator/auth.validator';
-import { login, register } from '@/controllers/auth/auth.controller';
+import { login, logout, refreshToken, register } from '@/controllers/auth/auth.controller';
 
 const app = new Hono()
     .post('/login', validate(loginSchema), login)
-    .post('/register', validate(registerSchema), register);
+    .post('/register', validate(registerSchema), register)
+    .post('/refresh', refreshToken)
+    .post('/logout', logout);
 
 export default app;
