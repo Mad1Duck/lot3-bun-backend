012b175cbce543ee3d05acd2b158fd5ce62f6924
RaihanArdianata <raihan.ardianata@gmail.com>
2025-06-17 15:22:54 +0700

    change to pinata

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,11 +4,13 @@ module.exports = {
       name: "lot3-backend",
       script: "bun",
       args: "run dev",
-      interpreter: "",        // let PM2 run it directly
       instances: 1,
       autorestart: true,
-      watch: true,
-      max_memory_restart: "1512M",
+      watch: false,
+      max_memory_restart: "0",
+      out_file: "./logs/out.log",
+      error_file: "./logs/error.log",
+      log_date_format: "YYYY-MM-DD HH:mm Z",
       env: {
         PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
         NODE_ENV: "development",
