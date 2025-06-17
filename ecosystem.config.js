bbbdd34a71ed9bd9d61802f7e104d77caf21b0e2
RaihanArdianata <raihan.ardianata@gmail.com>
2025-06-17 13:14:00 +0700

    add config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,19 +2,20 @@ module.exports = {
   apps: [
     {
       name: "lot3-backend",
-      script: "src/index.ts",
-      interpreter: "bun",         // ← use Bun, not ts-node
+      script: "bun",
+      args: "run dev",
+      interpreter: "",        // let PM2 run it directly
       instances: 1,
       autorestart: true,
       watch: true,
       max_memory_restart: "1512M",
       env: {
-        PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`, // Add "~/.bun/bin/bun" to PATH
+        PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
         NODE_ENV: "development",
         PORT: 8081,
       },
       env_production: {
-        PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`, // Add "~/.bun/bin/bun" to PATH
+        PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
         NODE_ENV: "production",
         PORT: 8082,
       },
