70373ba24cff9a8ddda5696869665fac6c081f8f
RaihanArdianata <raihan.ardianata@gmail.com>
2025-06-17 15:34:19 +0700

    change to pinata

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,12 +3,17 @@ module.exports = {
     {
       name: "lot3-backend",
       script: "bun",
-      args: ["run", "src/index.ts"],
-      interpreter: "",
+      args: ["run", "--hot", "src/index.ts"],
+
       instances: 1,
-      autorestart: false,
-      watch: false,  // matikan watch dari PM2 biar gak bentrok sama --hot bun
-      max_memory_restart: "0", // matikan limit dulu
+      autorestart: true,
+
+      // Nonaktifkan watch karena bun sudah handle hot reload
+      watch: false,
+
+      // Nonaktifkan max memory restart dulu, bisa diaktifkan kalau mau nanti
+      max_memory_restart: "0",
+
       env: {
         PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
         NODE_ENV: "development",
@@ -19,9 +24,13 @@ module.exports = {
         NODE_ENV: "production",
         PORT: 8082,
       },
+
       out_file: "./logs/out.log",
       error_file: "./logs/error.log",
-      log_date_format: "YYYY-MM-DD HH:mm Z",
+      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+      // Tambahan supaya PM2 lebih stabil:
+      max_restarts: 10,       // Batasi restart beruntun
+      restart_delay: 3000,    // Delay 3 detik antar restart
     },
   ],
 };
